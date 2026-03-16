9adcc373bed7c2b3968814539356bfc1d34cd1a0
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-16 09:04:27 +0100

    what the program prints/shows eventually

---
+++ src/final_result/fr.py
@@ -0,0 +1,8 @@
+#number of players wearing blue jersey
+print("Blue Team Players:", blue_count)
+#number of players wearing white jersey
+print("White Team Players:", white_count)
+
+os.makedirs("outputs/annotated_images", exist_ok=True)
+cv2.imwrite(f"outputs/annotated_images/{image_path}", img)
+cv2_imshow(img)

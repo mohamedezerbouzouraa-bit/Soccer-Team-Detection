ba404f8e2f7da500944dec0e0e3d919b1def148f
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-17 16:17:54 +0100

    extract_torso function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from src.config import BLUE_LOWER, BLUE_UPPER, TORSO_VERTICAL_RATIO, TORSO_HORIZONTAL_RATIO
 
+
 def extract_torso(player_img):
     h, w, _ = player_img.shape
     v_start = int(h * TORSO_VERTICAL_RATIO[0])

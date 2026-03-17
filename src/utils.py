fbc2484a9136ba8159210cbcb999038c53044d9c
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-17 23:39:01 +0100

    Extract height and width in extract_torso function
    Add height and width extraction for torso extraction.

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from src.config import BLUE_LOWER, BLUE_UPPER, TORSO_VERTICAL_RATIO, TORSO_HORIZ
 
 
 def extract_torso(player_img):
+    
     h, w, _ = player_img.shape
     v_start = int(h * TORSO_VERTICAL_RATIO[0])
     v_end = int(h * TORSO_VERTICAL_RATIO[1])

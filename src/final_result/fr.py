53c62201fdd0aaf0f729ae7af82d2c3e75d7da4b
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-13 15:01:00 +0200

    Fix cv2_imshow call to ensure image display

--- src/final_result/fr.py
+++ src/final_result/fr.py
@@ -2,7 +2,6 @@
 print("Blue Team Players:", blue_count)
 #number of players wearing white jersey
 print("White Team Players:", white_count)
-
 os.makedirs("outputs/annotated_images", exist_ok=True)
 cv2.imwrite(f"outputs/annotated_images/{image_path}", img)
 cv2_imshow(img)
